7407ffd7c8221e5b2be161b9b8cbb390ad2c2581
marleneanton <the.marlene.anton@gmail.com>
2024-09-24 16:49:20 +0200

    adds character prop to each file, though introduces bug

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -7,8 +7,6 @@ export type CharacterCardProps = {
 }
 
 export default function CharacterCard(props: Readonly<CharacterCardProps>) {
-    // todo: Step 2: Create another route that navigates to the "CharacterDetailCard" component on the path "/characters/:id".
-    //  The CharacterDetailCard should receive a list of all characters as a prop, and based on the ID, the correct character should be selected and displayed.
     return (
         <Link to={"/character/" + props.character.id}>
             <div className="character-card">
--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,11 +1,15 @@
 import CharacterGallery from "../components/CharacterGallery.tsx";
 import {useState} from "react";
-import {characters} from "../Characters.ts";
+import {Character} from "../types/RickAndMortyCharacter.ts";
+export type CharacterListProps = {
+    characters: Character[];
+}
 
-export default function CharacterPage() {
+export default function CharacterPage(props:Readonly<CharacterListProps>) {
     const [searchText, setSearchText] = useState("");
-    const filteredCharacters = characters
+    const filteredCharacters = props.characters
         .filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()));
+
 
     return <>
         <input type="text" onChange={(e) => setSearchText(e.target.value)} placeholder="Search for a character"/>
